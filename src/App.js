63d26aba369e2244c914e0ae44a65352bc84165a
peckpeck20 <a1602411@myy.haaga-helia.fi>
2018-04-15 22:31:23 +0300

    data in table and sorting

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import MainTable from "./Components/MainTable";
+
+
+import CustomerTable from "./Components/CustomerTable";
+// import CustomerTable from "./Components/CustomerTable"
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      customers: null,
-      trainings : null,
+      customers: [],
+      trainings : [],
     }
     // This binding is necessary to make `this` work in the callback
     this.fetchData = this.fetchData.bind(this);
@@ -35,13 +38,14 @@ class App extends Component {
 
 
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        {/* <MainTable customers={this.state.customers}/> */}
+        <CustomerTable customers={this.state.customers}/>
       </div>
     );
   }
--- src/Components/CustomerTable.js
+++ src/Components/CustomerTable.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import ReactTable from "react-table";
 import "react-table/react-table.css";
 
-export default class MainTable extends Component {
+export default class CustomerTable extends Component {
   render() {
     const columns = [
       {
@@ -33,27 +33,21 @@ export default class MainTable extends Component {
       {
         Header: "phone",
         accessor: "phone"
-      }]
-
-     const tt = this.props.customers
- console.log('====================================');
- console.log(tt);
- console.log('====================================');
-
-    // const data = [{
-    //   name: 'Tanner Linsley',
-    //   age: 26,
-    //   friend: {
-    //     name: 'Jason Maurer',
-    //     age: 23,
-    //   }
-    // }]
-  
+      }
+    ];
 
     return (
-      <div>
-        <ReactTable data={tt} columns={columns} />
-      </div>
+      // <div>
+      //   <h1>hola</h1>
+
+      // </div>
+
+      <ReactTable
+        data={this.props.customers}
+        columns={columns}
+        defaultPageSize={30}
+        filterable={true}
+      />
     );
   }
 }
